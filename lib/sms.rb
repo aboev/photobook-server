748bdd02d38e73c650c278c60286092b492ffdab
Aleksey Boev <boev.aleksey@gmail.com>
2018-02-19 21:43:22 +0300

    Implement custom sms gateway support

--- lib/sms.rb
+++ lib/sms.rb
@@ -8,9 +8,16 @@ class SMSGateway
     log = Logger.new 'log/sms.log'
 
     msg = URI.encode("Hello. Your Photobook verification code is: " + code.to_s)
-    uri = 'http://smsc.ru/sys/send.php?login=' + APP_CONFIG['smsc_login'] + '&psw=' + APP_CONFIG['smsc_pw'] + '&phones=' + number + '&mes=' + msg
-    open(uri)
-    log.debug("Sending sms verification code to " + number)
+
+    if APP_CONFIG['sms_gate'] == 0
+      uri = 'http://smsc.ru/sys/send.php?login=' + APP_CONFIG['smsc_login'] + '&psw=' + APP_CONFIG['smsc_pw'] + '&phones=' + number + '&mes=' + msg
+      open(uri)
+      log.debug("Sending sms verification code to " + number)
+    else
+      system './lib/sms.sh', number, msg
+      log.debug("Sending sms verification code to " + number + " using custom gateway")
+    end
+
   end
 
 end
---
+++ lib/sms.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Implement custom sms gateway here
+# Use following arguments:
+#   $1 - user phone number
+#   $2 - text message
+# Example: 
+#   curl "http://<your gateway address>?number=$1&message=$2"
